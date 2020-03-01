d0eda7f54d6e6f63eac97933958f3c42987cbcde
Daniel Stenberg <daniel@haxx.se>
2020-03-01 16:26:39 +0100

    fixup remove src[14] from the printf

--- lib/version.c
+++ lib/version.c
@@ -207,8 +207,7 @@ char *curl_version(void)
   *fmtp = 0;
 
   msnprintf(out, sizeof(out), format, src[0], src[1], src[2], src[3], src[4],
-            src[5], src[6], src[7], src[8], src[9], src[10], src[11], src[13],
-            src[14]);
+            src[5], src[6], src[7], src[8], src[9], src[10], src[11], src[13]);
 
   return out;
 }
