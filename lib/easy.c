e3a3313baa52fec1a61d70e3041a2303c26f6a47
Daniel Stenberg <daniel@haxx.se>
2020-03-02 09:26:59 +0100

    global_init: use _Atomic for the initialize counter
    This is a C11 feature that when available makes the init reference
    counter atomic and thread-safe.

--- lib/easy.c
+++ lib/easy.c
@@ -85,8 +85,13 @@
 
 void Curl_version_init(void);
 
+/* _Atomic is a C11 feature */
+#ifndef HAVE_ATOMIC
+#define _Atomic
+#endif
+
 /* true globals -- for curl_global_init() and curl_global_cleanup() */
-static unsigned int  initialized;
+static _Atomic unsigned int  initialized;
 static long          init_flags;
 
 /*
